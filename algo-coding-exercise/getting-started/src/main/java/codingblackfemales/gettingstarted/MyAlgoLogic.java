bee3ae02f385d44b5ea8531218fd4572cb0e51ae
AdelabuAderonke <adelabu.aderonke9638@gmail.com>
2023-08-20 19:04:36 +0100

    Create child order,container, order services

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -1,10 +1,24 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.action.Action;
+import codingblackfemales.action.CreateChildOrder;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.container.Actioner;
+import codingblackfemales.container.AlgoContainer;
+import codingblackfemales.container.RunTrigger;
+import codingblackfemales.sequencer.DefaultSequencer;
+import codingblackfemales.sequencer.Sequencer;
+import codingblackfemales.sequencer.event.OrderEventListener;
+import codingblackfemales.sequencer.net.TestNetwork;
+import codingblackfemales.service.MarketDataService;
+import codingblackfemales.service.OrderService;
+import codingblackfemales.sotw.ChildOrder;
+import codingblackfemales.sotw.OrderState;
 import codingblackfemales.sotw.SimpleAlgoState;
 import codingblackfemales.util.Util;
+import messages.order.PendingOrderDecoder;
+import messages.order.Side;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,12 +32,20 @@ public class MyAlgoLogic implements AlgoLogic {
         var orderBookAsString = Util.orderBookToString(state);
 
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
+        final TestNetwork network = new TestNetwork();
+        final Sequencer sequencer = new DefaultSequencer(network);
+        final RunTrigger runTrigger = new RunTrigger();
+        final Actioner actioner = new Actioner(sequencer);
 
-        /********
-         *
-         * Add your logic here....
-         *
-         */
+        AlgoContainer container = new AlgoContainer(new MarketDataService(runTrigger), new OrderService(runTrigger), runTrigger, actioner);
+        CreateChildOrder order = new CreateChildOrder(Side.BUY,3,100);
+        ChildOrder order1 = new ChildOrder(Side.SELL,1,3,100, OrderState.PENDING);
+        OrderService orderService = container.getOrderService();
+        orderService.updateState(order1,0);
+        /*public void onPendingOrder(final PendingOrderDecoder pending) {
+            updateState(find(pending.orderId()), OrderState.PENDING);
+            triggerRun();
+        }*/
 
         return NoAction.NoAction;
     }
