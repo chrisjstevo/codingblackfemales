badd25d01f56de73db1c6268c75bf07b1688db24
theifeoma <if.comokafor@ymail.com>
2023-08-21 21:04:40 +0100

    AlgoBackTest implemented

--- algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
+++ algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
@@ -2,6 +2,9 @@ package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
 import org.junit.Test;
+import codingblackfemales.sotw.ChildOrder;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * This test plugs together all of the infrastructure, including the order book (which you can trade against)
@@ -27,19 +30,23 @@ public class MyAlgoBackTest extends AbstractAlgoBackTest {
         //create a sample market data tick....
         send(createTick());
 
-        //ADD asserts when you have implemented your algo logic
-        //assertEquals(container.getState().getChildOrders().size(), 3);
-
-        //when: market data moves towards us
-        send(createTick2());
-
         //then: get the state
         var state = container.getState();
 
+        //ADD asserts when you have implemented your algo logic
+        assertEquals(container.getState().getChildOrders().size(), 5);
+
+        //when: market data moves towards us
+        System.out.println("Market moves to us!");
+        send(createTick2());
+
         //Check things like filled quantity, cancelled order count etc....
-        //long filledQuantity = state.getChildOrders().stream().map(ChildOrder::getFilledQuantity).reduce(Long::sum).get();
-        //and: check that our algo state was updated to reflect our fills when the market data
-        //assertEquals(225, filledQuantity);
+        long filledQuantity = state.getChildOrders().stream().map(ChildOrder::getFilledQuantity).reduce(Long::sum).get();
+        //long setPrice = state.getChildOrders().stream().map(ChildOrder::getPrice).reduce(Long::sum).get();
+
+        //check that created child orders have been cancelled
+        assertEquals(0, filledQuantity);
+        //assertEquals(450, setPrice);
     }
 
 }
