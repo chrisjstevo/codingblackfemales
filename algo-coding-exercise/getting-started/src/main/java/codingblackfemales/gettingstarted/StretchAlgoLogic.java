81494bca823ed483ae3f27ed1f9565d01bce1001
theifeoma <if.comokafor@ymail.com>
2023-08-22 00:29:20 +0100

    New code in StretchAlgoLogic

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/StretchAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/StretchAlgoLogic.java
@@ -1,10 +1,14 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.action.Action;
+import codingblackfemales.action.CreateChildOrder;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.sotw.ChildOrder;
 import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.sotw.marketdata.AskLevel;
 import codingblackfemales.util.Util;
+import messages.order.Side;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +23,23 @@ public class StretchAlgoLogic implements AlgoLogic {
         var orderBookAsString = Util.orderBookToString(state);
 
         logger.info("[STRETCHALGO] The state of the order book is:\n" + orderBookAsString);
+
+        //create a sell
+        final AskLevel askLevel = state.getAskAt(0);
+
+        long askPrice = askLevel.price;
+        long askQuantity = askLevel.quantity;
+
+        //ChildOrder newOrder = new ChildOrder(Side.BUY, 1, 50L, 105, 1);
+        CreateChildOrder childOrder = new CreateChildOrder(Side.SELL, 50, 105);
+
+        if(newOrder.getPrice() > askPrice){
+            //match order
+            //clear order how?
+        }
+        else{
+            //do nothing
+        }
 
 
 
