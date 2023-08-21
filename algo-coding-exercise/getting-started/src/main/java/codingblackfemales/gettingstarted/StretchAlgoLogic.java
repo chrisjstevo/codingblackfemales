71f9aa74961ce47901b722195f7a8820af8527bd
theifeoma <if.comokafor@ymail.com>
2023-08-21 21:41:40 +0100

    Logger inclusion in StretchAlgoLogic

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/StretchAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/StretchAlgoLogic.java
@@ -4,10 +4,23 @@ import codingblackfemales.action.Action;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
 import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.util.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StretchAlgoLogic implements AlgoLogic {
+
+    private static final Logger logger = LoggerFactory.getLogger(StretchAlgoLogic.class);
     @Override
     public Action evaluate(SimpleAlgoState state) {
+
+        logger.info("[STRETCHALGO] In Algo Logic....");
+
+        var orderBookAsString = Util.orderBookToString(state);
+
+        logger.info("[STRETCHALGO] The state of the order book is:\n" + orderBookAsString);
+
+
 
         return NoAction.NoAction;
     }
