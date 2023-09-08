b6492589d5b9e091c60a075daa23a35bd3f7543a
Emem Attah <ememattah7@hotmail.co.uk>
2023-09-08 19:04:24 +0100

    First Draft - Algo Logic & Algo Test attempting to createTick to send market data into files.

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -5,12 +5,51 @@ import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
 import codingblackfemales.sotw.SimpleAlgoState;
 import codingblackfemales.util.Util;
+import messages.marketdata.*;
+import org.agrona.concurrent.UnsafeBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.nio.ByteBuffer;
+import codingblackfemales.sequencer.DefaultSequencer;
+import codingblackfemales.sequencer.Sequencer;
+import codingblackfemales.sequencer.RealSequencer;
+
 
 public class MyAlgoLogic implements AlgoLogic {
 
     private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
+
+    private final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
+    private final BookUpdateEncoder bookUpdateEncoder = new BookUpdateEncoder();
+
+    private UnsafeBuffer createTick2(){
+        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
+
+        //write the encoded output to the direct buffer
+        bookUpdateEncoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
+        //set the fields to desired values
+        bookUpdateEncoder.venue(Venue.XLON);
+        bookUpdateEncoder.instrumentId(123L);
+        bookUpdateEncoder.source(Source.STREAM);
+
+        bookUpdateEncoder.bidBookCount(3)
+                .next().price(100L).size(100L)
+                .next().price(96L).size(200L)
+                .next().price(93L).size(300L);
+
+        bookUpdateEncoder.askBookCount(4)
+                .next().price(101L).size(101L)
+                .next().price(115L).size(200L)
+                .next().price(120L).size(5000L)
+                .next().price(130L).size(5000L);
+
+        bookUpdateEncoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
+
+        return directBuffer;
+    }
+
+
 
     @Override
     public Action evaluate(SimpleAlgoState state) {
@@ -18,12 +57,18 @@ public class MyAlgoLogic implements AlgoLogic {
         var orderBookAsString = Util.orderBookToString(state);
 
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
-
         /********
+         ** .
+         *  * You can tick in market data messages by creating new versions of createTick() (ex. createTick2, createTickMore etc..)
+         *  * You should then add behaviour to your algo to respond to that market data by creating or cancelling child orders.
+         *  *
          *
-         * Add your logic here....
+         *
+         *
+         *
          *
          */
+
 
         return NoAction.NoAction;
     }
--- algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
+++ algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
@@ -1,7 +1,27 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.algo.PassiveAlgoLogic;
+import codingblackfemales.container.Actioner;
+import codingblackfemales.container.AlgoContainer;
+import codingblackfemales.container.RunTrigger;
+import codingblackfemales.orderbook.OrderBook;
+import codingblackfemales.orderbook.channel.MarketDataChannel;
+import codingblackfemales.orderbook.channel.OrderChannel;
+import codingblackfemales.orderbook.consumer.OrderBookInboundOrderConsumer;
+import codingblackfemales.sequencer.DefaultSequencer;
+import codingblackfemales.sequencer.Sequencer;
+import codingblackfemales.sequencer.consumer.LoggingConsumer;
+import codingblackfemales.sequencer.net.TestNetwork;
+import codingblackfemales.service.MarketDataService;
+import codingblackfemales.service.OrderService;
+import messages.marketdata.*;
+import org.agrona.concurrent.UnsafeBuffer;
 import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+import static org.junit.Assert.assertEquals;
 
 
 /**
@@ -16,20 +36,79 @@ import org.junit.Test;
  */
 public class MyAlgoTest extends AbstractAlgoTest {
 
+    private final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
+    private final BookUpdateEncoder encoder = new BookUpdateEncoder();
+
+    private AlgoContainer container;
+
     @Override
     public AlgoLogic createAlgoLogic() {
         //this adds your algo logic to the container classes
         return new MyAlgoLogic();
     }
 
+    @Override
+    public Sequencer getSequencer() {
+        final TestNetwork network = new TestNetwork();
+        final Sequencer sequencer = new DefaultSequencer(network);
+
+        final RunTrigger runTrigger = new RunTrigger();
+        final Actioner actioner = new Actioner(sequencer);
+
+        final MarketDataChannel marketDataChannel = new MarketDataChannel(sequencer);
+        final OrderChannel orderChannel = new OrderChannel(sequencer);
+        final OrderBook book = new OrderBook(marketDataChannel, orderChannel);
+
+        final OrderBookInboundOrderConsumer orderConsumer = new OrderBookInboundOrderConsumer(book);
+
+        container = new AlgoContainer(new MarketDataService(runTrigger), new OrderService(runTrigger), runTrigger, actioner);
+        //set my algo logic
+        container.setLogic(new MyAlgoLogic());
+
+        network.addConsumer(new LoggingConsumer());
+        network.addConsumer(book);
+        network.addConsumer(container.getMarketDataService());
+        network.addConsumer(container.getOrderService());
+        network.addConsumer(orderConsumer);
+        network.addConsumer(container);
+
+        return sequencer;
+    }
+    private UnsafeBuffer createTick2(){
+        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
+
+        //write the encoded output to the direct buffer
+        encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
+
+        //set the fields to desired values
+        encoder.venue(Venue.XLON);
+        encoder.instrumentId(123L);
+        encoder.source(Source.STREAM);
+
+        encoder.bidBookCount(3)
+                .next().price(95L).size(100L)
+                .next().price(93L).size(200L)
+                .next().price(91L).size(300L);
+
+        encoder.askBookCount(4)
+                .next().price(98L).size(501L)
+                .next().price(101L).size(200L)
+                .next().price(110L).size(5000L)
+                .next().price(119L).size(5600L);
+
+        encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
+
+        return directBuffer;
+    }
 
     @Test
     public void testDispatchThroughSequencer() throws Exception {
 
         //create a sample market data tick....
-        send(createTick());
+        send(createTick2());
 
         //simple assert to check we had 3 orders created
-        //assertEquals(container.getState().getChildOrders().size(), 3);
+        assertEquals(container.getState().getChildOrders().size(), 3);
     }
 }
