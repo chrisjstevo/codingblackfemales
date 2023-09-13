01947572a3b7e02425860b410f99be3b70d30914
Emem Attah <ememattah7@hotmail.co.uk>
2023-09-13 17:04:53 +0100

    Second Attempt- Modified Algo Test to remedy the error a NullPointerException Error.

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -72,4 +72,5 @@ public class MyAlgoLogic implements AlgoLogic {
 
         return NoAction.NoAction;
     }
+
 }
--- algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoLogicTest.java
+++ algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoLogicTest.java
@@ -1,13 +1,28 @@
 package codingblackfemales.gettingstarted;
 
+
+import codingblackfemales.action.NoAction;
+import codingblackfemales.algo.AddCancelAlgoLogic;
+import codingblackfemales.container.Actioner;
+import codingblackfemales.container.AlgoContainer;
+import codingblackfemales.container.RunTrigger;
+import codingblackfemales.service.MarketDataService;
+import codingblackfemales.service.OrderService;
 import codingblackfemales.sotw.SimpleAlgoState;
 import codingblackfemales.sotw.SimpleAlgoStateImpl;
+import org.junit.Before;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 class MyAlgoLogicTest {
-  public MyAlgoLogic algoLogic = new MyAlgoLogic();
-  pub
+
+    MyAlgoLogic algoLogic = new MyAlgoLogic();
+    RunTrigger runTrigger;
+    MarketDataService dataService;
+    OrderService orderService;
+    Actioner actioner;
+    AlgoContainer container;
+    SimpleAlgoState state;
 
 
     @org.junit.jupiter.api.Test
@@ -17,9 +32,19 @@ class MyAlgoLogicTest {
 
     @org.junit.jupiter.api.Test
     void evaluate() {
-        SimpleAlgoState algoState = new SimpleAlgoStateImpl(null,null);
 
-        this.algoLogic.evaluate(algoState);
+        container = new AlgoContainer(new MarketDataService(runTrigger), new OrderService(runTrigger), runTrigger, actioner);
+        //set my algo logic
+        container.setLogic(new MyAlgoLogic());
+
+        this.state = new SimpleAlgoStateImpl(dataService, orderService);
+
+        this.algoLogic.evaluate(state);
+
+        var result   = NoAction.NoAction;
+
+        assertEquals(result,state);
+
 
     }
 }
