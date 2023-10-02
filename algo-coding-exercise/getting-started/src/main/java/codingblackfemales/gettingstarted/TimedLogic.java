c5ed866842989409b195505a2affe3ee42124d74
Sherelle <sherellie1@hotmail.com>
2023-10-02 01:51:49 +0100

    pseudo coded TimedLogic.java

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/TimedLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/TimedLogic.java
@@ -23,6 +23,13 @@ public class TimedLogic {
   Venue venue = new Venue();
 
       //   // Arraylist comprised of data harvested from the market every 15 minutes
+
+      //create an array for data harvested from market every 15 minutes
+    //   check is market open using isMarketOpen()
+    // use that to trigger schduleDataCollection() to push data to Arraylist.
+    // use checkForTrend(List<Long> marketMonitor) to trigger a buy action or a sell action. methods to be coded in next.
+
+
       List<Long>marketMonitor = new ArrayList<Long>();
 
         var totalOrderCount = state.getChildOrders().size();
@@ -36,11 +43,12 @@ public class TimedLogic {
         long price = farTouch.price;
 
       boolean marketIsOpen = true;
-      
+
       if(void isMarketOpen()){
         schduleDataCollection();
       }
 
+      public void checkForTrend(List<Long> marketMonitor){
       int consecutiveRises = 0;
       int consecutiveFalls = 0;
       int listSize = marketMonitor.size();
@@ -67,7 +75,7 @@ public class TimedLogic {
           // trigger buy action
         }
       }
-      
+    }
 
       public boolean isMarketOpen(boolean marketIsOpen){
         LocalDateTime currentDateTime = LocalDateTime.now();
@@ -104,69 +112,69 @@ public class TimedLogic {
 
       }
 
-      public void processMarketDate(){
+//       public void processMarketDate(){
 
-      }
-      public void collectData(){
-         // ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-        long marketUpdate = 0;
-      // you made this class you want it to update with the lastest data when market updates.
-      // you then want to push it to an array and check if last 3 updates were increaing, if so. we sell. if they decrease by e and price is lower than hwta we last sold at. buy again 
-        //new instance of schedule executor service 
-        Runnable harvestMarketData = () -> {
+//       }
+//       public void collectData(){
+//          // ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+//         ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+//         long marketUpdate = 0;
+//       // you made this class you want it to update with the lastest data when market updates.
+//       // you then want to push it to an array and check if last 3 updates were increaing, if so. we sell. if they decrease by e and price is lower than hwta we last sold at. buy again 
+//         //new instance of schedule executor service 
+//         Runnable harvestMarketData = () -> {
 
-          long newPrice = //grab new price from incoming market data
+//           long newPrice = //grab new price from incoming market data
 
 
-          // AlgoContainer algoContainer = new AlgoContainer(null, null, null, null);
+//           // AlgoContainer algoContainer = new AlgoContainer(null, null, null, null);
 
-          // do we think runTrigger is the signal to run the algo and if so can we use this to trigger running the algo every 15 min?
-          // RunTrigger runTrigger = new RunTrigger();
+//           // do we think runTrigger is the signal to run the algo and if so can we use this to trigger running the algo every 15 min?
+//           // RunTrigger runTrigger = new RunTrigger();
 
-          RandomMarketDataGenerator randomMarketDataGenerator = new RandomMarketDataGenerator(marketUpdate, null, marketUpdate, marketUpdate, marketUpdate);
+//           RandomMarketDataGenerator randomMarketDataGenerator = new RandomMarketDataGenerator(marketUpdate, null, marketUpdate, marketUpdate, marketUpdate);
 
-            // this is the task we want to execute every 15 minutes
-            // push data to the marketMonitor array.
-            // i want the result to go into ta variable called market update that can be added to the markeyMonitor arrayList
-            marketUpdate = ?;
+//             // this is the task we want to execute every 15 minutes
+//             // push data to the marketMonitor array.
+//             // i want the result to go into ta variable called market update that can be added to the markeyMonitor arrayList
+//             marketUpdate = ?;
            
            
-            scheduler.scheduleAtFixedRate(new.Task(marketUpdate), 0, 15, TimeUnit.MINUTES);
-            // scheduler.schedule(new Task(i), 10 - i,
-            //                    TimeUnit.SECONDS); 
-            marketMonitor.add(marketUpdate);
-      };
+//             scheduler.scheduleAtFixedRate(new.Task(marketUpdate), 0, 15, TimeUnit.MINUTES);
+//             // scheduler.schedule(new Task(i), 10 - i,
+//             //                    TimeUnit.SECONDS); 
+//             marketMonitor.add(marketUpdate);
+//       };
       
-  //     Runnable cancelCollection = () -> varName.cancel(false); scheduler.schedule(canceller, 8.5, Hours);
-  //   //   Runnable canceller = () -> beeperHandle.cancel(false);
-  //   //  scheduler.schedule(canceller, 1, HOURS);
-  //  };
+//   //     Runnable cancelCollection = () -> varName.cancel(false); scheduler.schedule(canceller, 8.5, Hours);
+//   //   //   Runnable canceller = () -> beeperHandle.cancel(false);
+//   //   //  scheduler.schedule(canceller, 1, HOURS);
+//   //  };
        
-  //       };
-    //  marketMonitor.add(marketUpdate);
-    public static void main(String[] args) {
-      // RandomMarketDataGenerator randomMarketDataGenerator = new RandomMarketDataGenerator(, null, marketUpdate, marketUpdate, marketUpdate);
-      // construct a venue and then parse it to random market generator:
+//   //       };
+//     //  marketMonitor.add(marketUpdate);
+//     public static void main(String[] args) {
+//       // RandomMarketDataGenerator randomMarketDataGenerator = new RandomMarketDataGenerator(, null, marketUpdate, marketUpdate, marketUpdate);
+//       // construct a venue and then parse it to random market generator:
 
-       Venue venue = new Venue(1);
-       venue.value();
+//        Venue venue = new Venue(1);
+//        venue.value();
 
-    RandomMarketDataGenerator randomMarketDataGenerator = new RandomMarketDataGenerator(5032, venue, 10, 13, 5);
+//     RandomMarketDataGenerator randomMarketDataGenerator = new RandomMarketDataGenerator(5032, venue, 10, 13, 5);
 
-    randomMarketDataGenerator.
-    // find / create market data object, do  a loop. if it increases 3 times execute a cell. 
-    // venue is intergers 1 london, 2paris, 3amstadam
+//     randomMarketDataGenerator.
+//     // find / create market data object, do  a loop. if it increases 3 times execute a cell. 
+//     // venue is intergers 1 london, 2paris, 3amstadam
   
-  }
+//   }
 
 
 
-  // at some point we would like to say 
-  if(marketMonitor index increses 3 times in a row){
-    new CreateChildOrder(Side.SELL, quantity, price);
-  }
-  if(marketsMonitor decreases 3 times in a row){
-    new CreateChildOrder(Side.BUY, quantity, price);
-  }
-}
+//   // at some point we would like to say 
+//   if(marketMonitor index increses 3 times in a row){
+//     new CreateChildOrder(Side.SELL, quantity, price);
+//   }
+//   if(marketsMonitor decreases 3 times in a row){
+//     new CreateChildOrder(Side.BUY, quantity, price);
+//   }
+// }
