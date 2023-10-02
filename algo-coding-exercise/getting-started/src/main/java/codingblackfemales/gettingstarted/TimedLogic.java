3474dcd00d70c4f1cf5250c2adbc5f2c333b2147
Sherelle <sherellie1@hotmail.com>
2023-10-03 00:55:29 +0100

    playing with the marketDataGenerator to get simpleFileMarketDataGenerator and some RandomMarketGenerator. may need to use jackson and an object mapper to get desired info... prices and quantities

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/TimedLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/TimedLogic.java
@@ -10,18 +10,61 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import codingblackfemales.action.Action;
 import codingblackfemales.action.CreateChildOrder;
 import codingblackfemales.action.NoAction;
+import codingblackfemales.algo.AlgoLogic;
 import codingblackfemales.container.AlgoContainer;
+import codingblackfemales.marketdata.gen.MarketDataGenerator;
 import codingblackfemales.marketdata.gen.RandomMarketDataGenerator;
+import codingblackfemales.marketdata.gen.SimpleFileMarketDataGenerator;
 import codingblackfemales.sotw.SimpleAlgoState;
 import codingblackfemales.sotw.marketdata.AskLevel;
+import codingblackfemales.util.Util;
 import messages.marketdata.Venue;
 import messages.order.Side;
 
-public class TimedLogic {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-  SimpleAlgoState
+public class TimedLogic implements AlgoLogic {
+
+  private static final Logger logger = LoggerFactory.getLogger(TimedLogic.class);
+  private SimpleFileMarketDataGenerator marketDataGenerator;
+
+   @Override
+      public Action evaluate(SimpleAlgoState state) {
+
+          var orderBookAsString = Util.orderBookToString(state);
+
+        logger.info("[TIMEDLOGIC] The state of the order book is:\n" + orderBookAsString);
+        // TODO Auto-generated method stub
+
+
+         List<Long>marketMonitor = new ArrayList<Long>();
+
+        var totalOrderCount = state.getChildOrders().size();
+
+        final var activeOrders = state.getActiveChildOrders();
+
+        final AskLevel farTouch = state.getAskAt(0);
+        // BidLevel level = state.getBidAt(i);
+        // AskLevel level = state.getAskAt(i);
+
+        //take as much as we can from the far touch....
+        long quantity = farTouch.quantity;
+        long latestPrices = 0;
+
+      boolean isMarketOpen = true;
+
+      if(isMarketOpen(isMarketOpen)){
+        schduleDataCollection(latestPrices, latestPrices);
+      }
+
+      }
+  // SimpleAlgoState
 
       //   // Arraylist comprised of data harvested from the market every 15 minutes
 
@@ -31,25 +74,94 @@ public class TimedLogic {
     // use checkForTrend(List<Long> marketMonitor) to trigger a buy action or a sell action. methods to be coded in next.
 
 
-      List<Long>marketMonitor = new ArrayList<Long>();
+     
 
-        var totalOrderCount = state.getChildOrders().size();
+      public boolean isMarketOpen(boolean isMarketOpen){
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DayOfWeek dayOfWeek = currentDateTime.getDayOfWeek();
+        LocalTime timeNow = LocalTime.now();
 
-        final var activeOrders = state.getActiveChildOrders();
+        if (dayOfWeek != DayOfWeek.SATURDAY || dayOfWeek != DayOfWeek.SUNDAY){
+          LocalTime marketOpen = LocalTime.of(8, 0);
+          LocalTime marketClosed = LocalTime.of(16, 30);
+          
+          if(timeNow.isBefore(marketOpen) || timeNow.isAfter(marketClosed)){
+            isMarketOpen = false;
+            logger.info("[TIMEDLOGIC] Market is closed");
 
-        final AskLevel farTouch = state.getAskAt(0);
-
-        //take as much as we can from the far touch....
-        long quantity = farTouch.quantity;
-        long latestPrices = 0l;
-
-      boolean isMarketOpen = true;
-
-      if(isMarketOpen(isMarketOpen)){
-        schduleDataCollection();
+          }
+        }
+        return isMarketOpen;
       }
 
-      public void checkForTrend(List<Long> marketMonitor){
+    //   public checkForTrend(List<Long> marketMonitor){
+    //   int consecutiveRises = 0;
+    //   int consecutiveFalls = 0;
+    //   int listSize = marketMonitor.size();
+
+    //   for(int i = listSize - 1; i >= 2; i--){
+    //     long currentPrice = marketMonitor.get(i);
+    //     long previousPrice = marketMonitor.get(i - 1);
+    //     long thirdPrice = marketMonitor.get(i - 2);
+
+    //     if(currentPrice < previousPrice && previousPrice < thirdPrice){
+    //       consecutiveRises++;
+    //       consecutiveFalls = 0;
+    //     }else if(currentPrice > previousPrice && previousPrice > thirdPrice){
+    //       consecutiveFalls++;
+    //       consecutiveRises = 0; //we rest the counter
+    //     }else{
+    //       consecutiveFalls = 0;
+    //       consecutiveRises = 0;
+    //     }
+
+    //     if(consecutiveRises >= 3){
+    //         logger.info("[TIMEDLOGIC] Market trend is rising selling " + quantity + "child orders at " + latestPrices);
+    //       new CreateChildOrder(Side.SELL, quantity, latestPrices);
+    //     }else if(consecutiveFalls >= 3){
+    //         logger.info("[TIMEDLOGIC] Market trend is falling buying " + quantity + "child orders at " + latestPrices);
+    //       new CreateChildOrder(Side.BUY, quantity, latestPrices);
+    //     }
+    //   }
+    // }
+
+long quantity = farTouch.quantity;
+        long latestPrices = 0;
+      
+      public void schduleDataCollection(/*long quantity, long latestPrices*/){
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        Runnable checkForTrend = () -> {
+          // what is the task we are looking to do at a scheduled time?
+          // get latest markets prices/ check latest market trends;
+
+          // find the method that gives you acces to receivin data from the market 
+          //  marketDataGenerator = new MarketDataGenerator();
+           marketDataGenerator = new SimpleFileMarketDataGenerator("src/test/resources/marketdata.json", new RandomMarketDataGenerator(instrumentId, venue, priceLevel, priceMaxDelta, 15)
+          //  may possibly need jacksonobjectMapper to retrieve prices from this api.
+          
+
+
+
+
+           checkForTrend(marketMonitor);
+
+          latestPrices = marketDataGenerator;/*this will hold the data that comesback from the market */;
+          //find the method;
+          
+          marketMonitor.add(latestPrices);
+
+          checkForTrend(List<Long>marketMonitor, latestPrices);
+
+          logger.info("[TIMEDLOGIC] latest price on the market is " + latestPrices);
+
+          
+        };
+          ScheduledFuture<?> scheduledFuture = scheduler.scheduleAtFixedRate(checkForTrend(marketMonitor), 0, 15, TimeUnit.MINUTES);
+
+      }
+
+      private Runnable checkForTrend(List<Long> marketMonitor) {
       int consecutiveRises = 0;
       int consecutiveFalls = 0;
       int listSize = marketMonitor.size();
@@ -71,52 +183,49 @@ public class TimedLogic {
         }
 
         if(consecutiveRises >= 3){
-            logger.info("[MYALGO] Market trend is rising selling " + quantity + "child orders at " + latestPrices);
+            logger.info("[TIMEDLOGIC] Market trend is rising selling " + quantity + "child orders at " + latestPrices);
           new CreateChildOrder(Side.SELL, quantity, latestPrices);
         }else if(consecutiveFalls >= 3){
-            logger.info("[MYALGO] Market trend is falling buying " + quantity + "child orders at " + latestPrices);
+            logger.info("[TIMEDLOGIC] Market trend is falling buying " + quantity + "child orders at " + latestPrices);
           new CreateChildOrder(Side.BUY, quantity, latestPrices);
         }
       }
+        return currentPrice;
+      }
+
+     
     }
 
-      public boolean isMarketOpen(boolean isMarketOpen){
-        LocalDateTime currentDateTime = LocalDateTime.now();
-        DayOfWeek dayOfWeek = currentDateTime.getDayOfWeek();
-        LocalTime timeNow = LocalTime.now();
+    RandomMarketDataGenerator
+    SimpleAlgoStateImpl
+    
 
-        if (dayOfWeek != DayOfWeek.SATURDAY || dayOfWeek != DayOfWeek.SUNDAY){
-          LocalTime marketOpen = LocalTime.of(8, 0);
-          LocalTime marketClosed = LocalTime.of(16, 30);
-          
-          if(timeNow.isBefore(marketOpen) || timeNow.isAfter(marketClosed)){
-            isMarketOpen = false;
-            logger.info("[MYALGO] Market is closed");
+        MarketDataGeneratorTest
+        RandomMarketDataGeneratorTest
+        //  algo-coding-exercise\algo\src\test\java\codingblackfemales\marketdata\api\MarketDataGeneratorTest.java
+        //  marketDataGenerator = new SimpleFileMarketDataGenerator("src/test/resources/marketdata.json", new RandomMarketDataGenerator(instrumentId, venue, priceLevel, priceMaxDelta, 15));
 
-          }
-        }
-        return isMarketOpen;
-      }
-      
-      public void schduleDataCollection(){
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+public class MarketDataGeneratorTest {
+    private SimpleFileMarketDataGenerator marketDataGenerator;
 
-        Runnable harvestMarketData = () -> {
-          // what is the task we are looking to do at a scheduled time?
-          // get latest markets prices/ check latest market trends;
-
-          // find the method that gives you acces to receivin data from the market 
-
-          latestPrices = newData;
-          //find the method;
-          logger.info("[MYALGO] latest price on the market is " + latestPrices);
-
-          marketMonitor.add(latestPrices);
-        };
-          ScheduledFuture<?> scheduledFuture = scheduler.scheduleAtFixedRate(harvestMarketData, 0, 15, TimeUnit.MINUTES);
-
-      }
+    @Before
+    public void setup() {
+        final long instrumentId = 1234;
+        final Venue venue = Venue.XLON;
+        final long priceLevel = 1000;
+        final long priceMaxDelta = 100;
+        marketDataGenerator = new SimpleFileMarketDataGenerator("src/test/resources/marketdata.json", new RandomMarketDataGenerator(instrumentId, venue, priceLevel, priceMaxDelta, 15));
     }
+
+    @Test
+    public void should_generate(){
+        marketDataGenerator.generate(1000);
+        marketDataGenerator.close();
+    }
+
+
+
+
 
 //       public void processMarketDate(){
 
