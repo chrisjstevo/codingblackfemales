ea291c26f6418f1a38b7cd4c1c4664ce3c38f89b
Esther Makinde <esthermakinde@hotmail.com>
2023-10-03 14:40:39 +0100

    Initial commit

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -1,12 +1,20 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.action.Action;
+import codingblackfemales.action.CancelChildOrder;
+import codingblackfemales.action.CreateChildOrder;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.sotw.ChildOrder;
 import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.sotw.marketdata.AskLevel;
+import codingblackfemales.sotw.marketdata.BidLevel;
 import codingblackfemales.util.Util;
+import messages.order.Side;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
 
 public class MyAlgoLogic implements AlgoLogic {
 
@@ -19,12 +27,71 @@ public class MyAlgoLogic implements AlgoLogic {
 
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
 
+
         /********
-         *
          * Add your logic here....
-         *
+         * Check the number of active orders. If active orders < 3, then create a child order. When there are 3 child orders, stop creating new child orders.
+         * BUY Child orders will be created on the passive side of the book to get a price as low as possible
+         * SELL Child orders using sniper logic, as long as the price is higher than the buy price for that stock?
+         * Cancel BUY child order to make space for SELL child order?
+         * Create hashmap to store the quantities and prices that they've been bought
+         * Cancel orders if going into loses, sell order
+         * Create method for cancel order and wait for it to be executed.
          */
 
-        return NoAction.NoAction;
+        final BidLevel nearTouch = state.getBidAt(0);
+        long bidQuantity = 50;
+        long bidPrice = nearTouch.price;
+
+        final AskLevel farTouch = state.getAskAt(0);
+        long askQuantity = farTouch.quantity;
+        long askPrice = farTouch.price;
+
+        var totalOrderCount = state.getChildOrders().size();
+        var activeOrderCount = state.getActiveChildOrders().size();
+
+
+//        logger.info("Filled price is: " + filledPrice);
+//        logger.info("Average filled price is: " + averageFilledPrice);
+
+
+        // If total order count is less than 20 generate and delete fake orders.
+        if (totalOrderCount < 20) {
+            if (activeOrderCount > 0) {
+
+                var option = state.getActiveChildOrders().stream().findFirst();
+
+                if (option.isPresent()) {
+                    var childOrder = option.get();
+                    return new CancelChildOrder(childOrder);
+                } else {
+                    return NoAction.NoAction;
+                }
+            } else {
+                return new CreateChildOrder(Side.BUY, bidQuantity, bidPrice);
+            }
+        }
+
+        long filledQuantity = state.getChildOrders().stream().map(ChildOrder::getFilledQuantity).reduce(Long::sum).get();
+        long filledPrice = state.getActiveChildOrders().stream().map(ChildOrder::getPrice).reduce(Long::sum).get();
+        long averageFilledPrice = filledPrice/activeOrderCount;
+
+        //create my 3 child orders on the buy side
+        if(activeOrderCount < 3){
+            //then keep creating a new one
+            logger.info("[PASSIVEALGO] Have:" + activeOrderCount + " active children, want 3, joining passive side of book with: " + bidQuantity + " @ " + bidPrice);
+            return new CreateChildOrder(Side.BUY, bidQuantity, bidPrice);
+//        } else if (activeOrderCount == 3 && averageFilledPrice < askPrice) {
+//            logger.info("[PASSIVEALGO] Have:" + activeOrderCount + " active children, creating sell order, joining passive side of book with: " + filledQuantity + " @ " + askPrice);
+//            return new CreateChildOrder(Side.SELL, filledQuantity,askPrice);
+        } else{
+            logger.info("[PASSIVEALGO] Done.");
+            return NoAction.NoAction;
+        }
+
+
+
+
     }
+
 }
--- algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
+++ algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
@@ -1,7 +1,13 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.sotw.ChildFill;
 import org.junit.Test;
+import codingblackfemales.sotw.ChildOrder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * This test plugs together all of the infrastructure, including the order book (which you can trade against)
@@ -17,6 +23,9 @@ import org.junit.Test;
  */
 public class MyAlgoBackTest extends AbstractAlgoBackTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(MyAlgoBackTest.class);
+
+
     @Override
     public AlgoLogic createAlgoLogic() {
         return new MyAlgoLogic();
@@ -28,18 +37,36 @@ public class MyAlgoBackTest extends AbstractAlgoBackTest {
         send(createTick());
 
         //ADD asserts when you have implemented your algo logic
-        //assertEquals(container.getState().getChildOrders().size(), 3);
+        assertEquals(container.getState().getChildOrders().size(), 22);
 
         //when: market data moves towards us
+        logger.info("market data moves towards us");
         send(createTick2());
 
         //then: get the state
+        logger.info("Get the state");
         var state = container.getState();
 
         //Check things like filled quantity, cancelled order count etc....
-        //long filledQuantity = state.getChildOrders().stream().map(ChildOrder::getFilledQuantity).reduce(Long::sum).get();
+        long filledQuantity = state.getChildOrders().stream().map(ChildOrder::getFilledQuantity).reduce(Long::sum).get();
+        logger.info("Filled quantity is: " + filledQuantity);
+
+        long quantity = state.getChildOrders().stream().map(ChildOrder::getQuantity).reduce(Long::sum).get();
+        logger.info("Quantity of all orders (filled and non-filled) is: " + quantity);
+
+        long price = state.getChildOrders().stream().map(ChildOrder::getPrice).reduce(Long::sum).get();
+        logger.info("Price is: " + price);
+
+        long filledPrice = state.getActiveChildOrders().stream().map(ChildOrder::getPrice).reduce(Long::sum).get();
+        var activeOrderCount = state.getActiveChildOrders().size();
+        long averageFilledPrice = filledPrice/activeOrderCount;
+        logger.info("Filled price is: " + filledPrice);
+        logger.info("Average filled price is: " + averageFilledPrice);
+
+
+
         //and: check that our algo state was updated to reflect our fills when the market data
-        //assertEquals(225, filledQuantity);
+        assertEquals(150, filledQuantity);
     }
 
 }
