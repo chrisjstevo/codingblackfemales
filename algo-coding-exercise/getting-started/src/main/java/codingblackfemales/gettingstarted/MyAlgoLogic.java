2f94afc6aeccd71d626e31d359d7347498cff82e
jenieb3 <117836200+jenieb3@users.noreply.github.com>
2023-10-04 16:47:02 +0100

    completed code to trading algo

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -4,13 +4,16 @@ import codingblackfemales.action.Action;
 import codingblackfemales.action.CreateChildOrder;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.sotw.ChildOrder;
 import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.sotw.marketdata.AskLevel;
 import codingblackfemales.sotw.marketdata.BidLevel;
 import codingblackfemales.util.Util;
 import messages.order.Side;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
 public class MyAlgoLogic implements AlgoLogic {
 
     private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
@@ -38,6 +41,30 @@ public class MyAlgoLogic implements AlgoLogic {
             return NoAction.NoAction;
         }
 
+    }
+}
+*/
+public class MyAlgoLogic implements AlgoLogic {
+    private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
+
+    @Override
+    public Action evaluate(SimpleAlgoState state) {
+        var orderBookAsString = Util.orderBookToString(state);
+        logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
+        final BidLevel topBid = state.getBidAt(0);
+        final AskLevel topAsk = state.getAskAt(0);
+        if (state.getChildOrders().size() < 3) {
+            logger.info("[MYALGO] creating child order.");
+            return new CreateChildOrder(Side.BUY, 100, topBid.price);
+        }
+        long filledQuantity = state.getChildOrders().stream().mapToLong(ChildOrder::getFilledQuantity).sum();
+        //if price is favourable place order
+        if (topAsk.price <= topBid.price && filledQuantity < 600) {
+            long quantityToFill = 600 - filledQuantity;
+            logger.info("[MYALGO] Filling order.");
+            return new CreateChildOrder(Side.SELL, quantityToFill, topAsk.price);
+        }
+        return NoAction.NoAction;
     }
 }
 
