278789d02a06e745b6de80b43cdc3df86ffdcde1
jenieb3 <117836200+jenieb3@users.noreply.github.com>
2023-10-04 12:30:55 +0100

    added logic for basic trading algo

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -1,16 +1,20 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.action.Action;
+import codingblackfemales.action.CreateChildOrder;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
 import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.sotw.marketdata.BidLevel;
 import codingblackfemales.util.Util;
+import messages.order.Side;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MyAlgoLogic implements AlgoLogic {
 
     private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
+    private int ordersCreated = 0;
 
     @Override
     public Action evaluate(SimpleAlgoState state) {
@@ -19,12 +23,21 @@ public class MyAlgoLogic implements AlgoLogic {
 
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
 
-        /********
-         *
-         * Add your logic here....
-         *
-         */
+        final BidLevel nearTouch = state.getBidAt(0);
+        long quantity = 100;
+        long price = nearTouch.price;
 
-        return NoAction.NoAction;
+        //until we have three child orders....
+        if(ordersCreated < 3 && state.getChildOrders().size() < 3){
+            //then keep creating a new one
+            logger.info("[MYALGO] Have:" + state.getChildOrders().size() + " children, want 3, joining passive side of book with: " + quantity + " @ " + price);
+            ordersCreated++;
+            return new CreateChildOrder(Side.BUY, quantity, price);
+        }else{
+            logger.info("[MYALGO] Have:" + state.getChildOrders().size() + " children, want 3, done.");
+            return NoAction.NoAction;
+        }
+
     }
 }
+
--- algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
+++ algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
@@ -2,6 +2,10 @@ package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
 
 
 /**
@@ -15,7 +19,7 @@ import org.junit.Test;
  *
  */
 public class MyAlgoTest extends AbstractAlgoTest {
-
+    private static final Logger logger = LoggerFactory.getLogger(MyAlgoTest.class);
     @Override
     public AlgoLogic createAlgoLogic() {
         //this adds your algo logic to the container classes
@@ -28,8 +32,7 @@ public class MyAlgoTest extends AbstractAlgoTest {
 
         //create a sample market data tick....
         send(createTick());
-
         //simple assert to check we had 3 orders created
-        //assertEquals(container.getState().getChildOrders().size(), 3);
+        assertEquals(container.getState().getChildOrders().size(), 3);
     }
 }
