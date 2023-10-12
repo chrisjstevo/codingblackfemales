e8e3b1eb4e6964c85d6edc6c2cdd34c6029a9bff
Naomi <naomi_bryant@hotmail.co.uk>
2023-10-12 13:52:38 +0100

    .git/COMMIT_EDITMSG
    My algo logic creates six child orders then cancels all child orders. My
    algo test checks if six child orders have been created then checks if
    there are any active child orders

--- algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-coding-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -1,30 +1,69 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.action.Action;
+import codingblackfemales.action.CancelChildOrder;
+import codingblackfemales.action.CreateChildOrder;
 import codingblackfemales.action.NoAction;
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.sotw.ChildOrder;
 import codingblackfemales.sotw.SimpleAlgoState;
+import codingblackfemales.sotw.marketdata.BidLevel;
 import codingblackfemales.util.Util;
+import messages.order.Side;
+
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MyAlgoLogic implements AlgoLogic {
 
     private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
-
-    @Override
+	@Override
     public Action evaluate(SimpleAlgoState state) {
 
         var orderBookAsString = Util.orderBookToString(state);
 
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
-
+        
+       
         /********
          *
          * Add your logic here....
          *
          */
+        final BidLevel nearTouch = state.getBidAt(0);
 
-        return NoAction.NoAction;
-    }
+        long quantity = 75;
+        long price = nearTouch.price;
+        final var activeOrders = state.getActiveChildOrders();
+
+        //until we have six child orders....
+        if (state.getChildOrders().size() < 6) 
+        {
+        	// then keep creating a new one
+        	logger.info("[MYALGO] Have:" + state.getChildOrders().size()
+        			+ " children, want 6, joining passive side of book with: " + quantity + " @ " + price);
+        	return new CreateChildOrder(Side.BUY, quantity, price);
+        } 
+        else 
+        {
+        	//Once six child orders have been created, start cancelling orders from the top of the book
+        	logger.info("[MYALGO] Have:" + state.getChildOrders().size() + " children, want 6, done.");
+        	
+        	final var option = activeOrders.stream().findFirst();
+        	if (option.isPresent()) 
+        	{
+        		var childOrder = option.get();
+        		logger.info("[ADDCANCELALGO] Cancelling order:" + childOrder);
+        		return new CancelChildOrder(childOrder);
+        	} 
+        	else 
+        	{
+        		return NoAction.NoAction;
+        	}
+ 
+        }
+        
+	}	
 }
--- algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
+++ algo-coding-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
@@ -1,7 +1,15 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.sotw.ChildOrder;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This test plugs together all of the infrastructure, including the order book (which you can trade against)
@@ -16,7 +24,8 @@ import org.junit.Test;
  *
  */
 public class MyAlgoBackTest extends AbstractAlgoBackTest {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(MyAlgoBackTest.class);
     @Override
     public AlgoLogic createAlgoLogic() {
         return new MyAlgoLogic();
@@ -28,18 +37,30 @@ public class MyAlgoBackTest extends AbstractAlgoBackTest {
         send(createTick());
 
         //ADD asserts when you have implemented your algo logic
-        //assertEquals(container.getState().getChildOrders().size(), 3);
-
+        logger.info("[MYALGO] Going to assert child = 6");
+        assertEquals(container.getState().getChildOrders().size(), 6);
+        
+        
+        logger.info("[MYALGO] Going to start cancelling orders");
         //when: market data moves towards us
         send(createTick2());
-
+        assertEquals(container.getState().getActiveChildOrders().size(), 0);
+        
+       
         //then: get the state
-        var state = container.getState();
+//        var state = container.getState();
 
         //Check things like filled quantity, cancelled order count etc....
         //long filledQuantity = state.getChildOrders().stream().map(ChildOrder::getFilledQuantity).reduce(Long::sum).get();
         //and: check that our algo state was updated to reflect our fills when the market data
         //assertEquals(225, filledQuantity);
+//        try {
+//        	var firstOrder = state.getChildOrders().stream().findFirst();
+//        	System.out.println("THIS IS THE FIRST ORDER " + firstOrder);
+//        } catch (Exception e){
+//        	System.out.println("OOOOOOOOOOOPPPPPPPPPSSSSSSSSS");
+//        }
+        
     }
 
 }
